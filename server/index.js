87282efb9148f5e28fa4712278524b9573098a4b
Akash290104 <mishra.ak801@gmail.com>
2025-02-23 13:50:25 +0530

    vercel live link

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://learning-management-system-lms-gray.vercel.app",
   methods: ["GET", "POST", "DELETE", "PUT"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
